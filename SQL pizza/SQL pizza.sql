e11597a784c69c9a0779e290de01ed3ef4a2016c
KAUE DOS SANTOS ANDRADE <082220027@faculdade.cefsa.edu.br>
2024-10-28 15:56:09 -0300

    Stored procedures genéricas e da tabela pizza criadas.

--- SQL pizza/SQL pizza.sql
+++ SQL pizza/SQL pizza.sql
@@ -1,5 +1,88 @@
+use Pizzaria
+GO
+
 create table tbPizza
-(id int not null primary key, descricao varchar(100) not null )
+(id int not null primary key identity, descricao varchar(100) not null )
 
 create table tbIngredientesPizza 
 (id int not null primary key, pizzaId int not null, descricao varchar(100) not null )
+
+/*----------------------------------------------------------------------------------*/
+                              --Procedures genéricas--
+/*----------------------------------------------------------------------------------*/
+
+create or alter procedure spDeletar(
+	@id int,
+	@tabela varchar(max)
+)
+as
+begin
+	declare @sql varchar(max)
+	set @sql = 'delete from ' + @tabela + ' where id = ' +  cast( @id as varchar(max))
+	exec(@sql)
+end
+go
+
+create or alter procedure spConsulta(
+	@id int,
+	@tabela varchar(max)
+)
+as
+begin
+	declare @sql varchar(max)
+	set @sql = 'select * from ' + @tabela + ' where id = ' + cast( @id as varchar(max))
+	exec(@sql)
+end
+go
+
+create or alter procedure spListagem(
+	@tabela varchar(max)
+)
+as
+begin
+	declare @sql varchar(max)
+	set @sql = 'select * from ' + @tabela
+	exec(@sql)
+end
+go
+
+create or alter procedure spIdAutomatico(
+	@tabela varchar(max)
+)
+as
+begin
+	exec('select isnull(max(id)+1,1) as MAIOR from ' + @tabela) 
+end
+go
+
+
+/*----------------------------------------------------------------------------------*/
+					 --Procedures específicas da tabela pizza--
+/*----------------------------------------------------------------------------------*/
+
+create or alter procedure spInserirPizza(
+	@descricao varchar(max))
+as
+begin
+	insert into tbPizza(descricao)
+	values (@descricao)
+end
+
+go
+
+
+create or alter procedure spEditarPizza(
+	@Id int,
+	@descricao varchar(50)
+)
+as
+begin
+	update tbPizza set descricao = @descricao
+	where id = @Id
+end
+go
+
+/*----------------------------------------------------------------------------------*/
+					--Procedures específicas da tabela ingredientes--
+/*----------------------------------------------------------------------------------*/
+
